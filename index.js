6cac500050f2ecfec3aac8094ec25771803e9778
GooX <goox@Temptation.local>
2017-12-28 14:21:52 +0400

    some cleanup

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function authenticate() {
     })
     .then(() => log.info('Ready for commands'))
     .then(() => postDataSocket(socket, '/play', 'application/x-apple-binary-plist', plist.bplistCreator({
-        'Content-Location': 'http://data04-cdn.datalock.ru/fi2lm/053b096c10388c7357164adc532efffc/7f_A.Place.To.Call.Home.S05E01.720p.HDTV.ColdFilm.Ru.a0.22.11.17.mp4',
+        'Content-Location': '',
         'Start-Position': 0.5
       }), true))
     .then(() => {
--- playlist.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	'season 1': 'http://data04-cdn.datalock.ru/fi2lm/053b096c10388c7357164adc532efffc/7f_A.Place.To.Call.Home.S05E01.720p.HDTV.ColdFilm.Ru.a0.22.11.17.mp4',
-  'skiing': ''
-};
